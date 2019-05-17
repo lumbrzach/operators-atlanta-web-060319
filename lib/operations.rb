a8828fccf1af88346bc22f81fb20bc64645e934e
Zach Parker <parkerz410@gmail.com>
2019-05-17 15:38:52 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,7 +1,5 @@
 def unsafe?(speed)
- if speed < 40 == true
-   true
- elsif speed > 60 == true
+ if speed < 40 || speed > 60
    true
  else
    false
@@ -10,5 +8,5 @@ end
 
 
 def not_safe?(speed)
-	
+	if speed < 40 || speed > 60 ? true : false 
 end
